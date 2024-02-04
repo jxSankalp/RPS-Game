2d0fa6b21d0c9362e3ca120bad1c1c0517c2d986
Sankalp Jain <sankalpjain2006@gmail.com>
2024-02-04 03:01:48 -0800

    Change in JS and CSS

--- script.js
+++ script.js
@@ -1,15 +1,25 @@
 
-function getRandomNumber() {
+function hideOthers(clickedImage) {
+    var images = document.querySelectorAll(".image");
+    images.forEach(function (image) {
+        if (image !== clickedImage) {
+            image.style.display = "none";
+        }
+    }
+    );
+}
 
+function getRandomNumber() {
     // 1 = rock
     // 2 = paper
     // 3 = scissor
+    const cryptoRandomArray = new Uint32Array(1);
+    crypto.getRandomValues(cryptoRandomArray);
 
-    const randomDecimal = Math.random();
+    // Generate a random number between 1 and 3
+    const cryptoRandomNumber = (cryptoRandomArray[0] % 3) + 1;
 
-    const randomNumber = Math.floor(randomDecimal * 3) + 1;
-
-    return randomNumber;
+    return cryptoRandomNumber;
 }
 
 function checking(clickedImage, randomValue) {
@@ -56,36 +66,59 @@ function checking(clickedImage, randomValue) {
 }
 
 function show_result(result) {
-
-    if (result == 1) {
+    if (result === 1) {
         var temp = document.getElementById("won");
-
         temp.style.display = "block";
     }
-    else if (result == 0) {
-        var temp = document.getElementById("lost");
-
+    else if (result === -1) {
+        var temp = document.getElementById("tie");
         temp.style.display = "block";
     }
     else {
-        var temp = document.getElementById("tie");
-
+        var temp = document.getElementById("lost");
         temp.style.display = "block";
     }
 }
 
 function onImageClick(clickedImage) {
-    var images = document.querySelectorAll(".image");
+    const images = document.querySelectorAll(".image");
+    var randomValue = getRandomNumber();
 
     images.forEach(function (image) {
         if (image !== clickedImage) {
             image.style.display = "none";
+        } else {
+            clickedImage.style.display = "block";
+
+            // Create a new image for the opponent's choice
+            const opponentImage = document.createElement("img");
+            opponentImage.classList.add("oppoimage");
+            opponentImage.style.display = "block";
+
+            // Set the source based on the random value
+            if (randomValue === 1) {
+                opponentImage.src = "obsidian.png";
+            } else if (randomValue === 2) {
+                opponentImage.src = "file.png";
+            } else {
+                opponentImage.src = "scissor.png";
+            }
+
+            // Replace the loader with the opponent's image
+            const loader = document.querySelector(".loader");
+            loader.replaceWith(opponentImage);
+            // loader.style.display = "none";
         }
     });
 
-    const randomValue = getRandomNumber();
-    const result = checking(clickedImage, randomValue);
+    var result = checking(clickedImage, randomValue);
     show_result(result);
 }
+
+function restart() {
+
+    location.reload();
+}
+
 
 
